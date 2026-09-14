b18a60f93f4469732d39fb796991478f4adb6a32
bhq658@myy.haaga-helia.fi <bhq658@myy.haaga-helia.fi>
2026-09-14 23:12:34 +0300

    neljäs

--- demo/src/main/java/bookstore/demo/BookController.java
+++ demo/src/main/java/bookstore/demo/BookController.java
@@ -3,6 +3,8 @@ package bookstore.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
@@ -22,5 +24,26 @@ public class BookController {
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+    // Show add book form
+    @GetMapping("/addbook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    // Save new book
+    @PostMapping("/savebook")
+    public String saveBook(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+    // Delete book
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+        return "redirect:/booklist";
     }
 }
