711c36c786325196520f637b3e3369e7c9e47bcd
bhq658@myy.haaga-helia.fi <bhq658@myy.haaga-helia.fi>
2026-09-14 23:01:23 +0300

    kolmas

--- demo/src/main/java/bookstore/demo/BookController.java
+++ demo/src/main/java/bookstore/demo/BookController.java
@@ -1,13 +1,26 @@
 package bookstore.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
 
+    private final BookRepository repository;
+
+    public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/index")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 }
